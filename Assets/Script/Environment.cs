d70ec060d764e85f2d78c8bf5277f16ead5a2e10
kitekii <tim720719@163.com>
2018-02-23 23:24:04 +0800

    Init Game Repo

---
+++ Assets/Script/Environment.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace Caress
+{
+    public class Environment : MonoBehaviour
+    {
+        Map defaultMap;
+        // Use this for initialization
+        void Start()
+        {
+            defaultMap = new Map("map01.json");
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
---
+++ Assets/Script/MapGenerator.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using LitJson;
+
+namespace Caress
+{
+    public class Map {
+        public Map(string Path)
+        {
+            this.mapName = Path;
+            this.Points = this.parseMapByName(Path);
+        }
+        public string mapName;
+        public List<Point> Points;
+        public static List<Point> parseMapByName(string Path)
+        {
+            return JsonMapper.ToObject<List<Point>>(new JsonReader(File.ReadAllText(path))); ;
+        }
+        Point getPointById(int id) {
+            foreach (Point x in Points)
+            {
+                if (id == x.id) {
+                    return x;
+                }
+            }
+            return new Point();
+        }
+    }
+    public class Point {
+        /**
+         * OnlySigned
+         **/
+        public int id;
+        /**
+         * in Range of (0,500)
+         * GameScene resize to (0,126)
+         **/
+        public int x;
+        public int y;
+        /**
+         * linked Points' id
+         * reffer to Map.getPointById(int id)
+         **/
+        public List<int> linked;
+    }
+}
